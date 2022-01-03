3e936eee50e02f3b2f4941306efa7d424f696fa1
Marlene Kreß <marlene.kress@gmx.de>
2022-01-03 11:18:08 +0100

    Update parser.py

--- exasol_sphinx_github_pages_generator/parser.py
+++ exasol_sphinx_github_pages_generator/parser.py
@@ -16,6 +16,7 @@ class Parser:
         parser.add_argument('--source_branch', type=str,
                             default="main",
                             help='current branch')
+        # TODO better defaults
         parser.add_argument('--source_dir', type=str, default="./doc/", help="The doc directory your documentation files and conf.py reside in")
         parser.add_argument('--module_path', type=str, default="../module_name", help="The path to the module the docu is beeing generated for")
         self.args = parser.parse_args(argv)
