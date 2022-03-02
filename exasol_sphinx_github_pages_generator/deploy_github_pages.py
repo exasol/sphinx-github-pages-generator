5fff05281a1de0711efed294a90d6785a89d5dca
MarleneKress79789 <marlene.kress@gmx.de>
2022-03-02 13:50:54 +0100

    fixed cleandoc output

--- exasol_sphinx_github_pages_generator/deploy_github_pages.py
+++ exasol_sphinx_github_pages_generator/deploy_github_pages.py
@@ -12,25 +12,27 @@ def deploy_github_pages(argv):
     original_workdir = os.getcwd()
     source_dir = args.source_dir
     current_commit_id = run(["git", "rev-parse", "HEAD"], capture_output=True, text=True, check=True)
-    print(cleandoc("Commandline parameter \n"
-                   f"source_dir= {source_dir} \n"
-                   f"module_path= {str(args.module_path)} \n"
-                   f"TARGET_BRANCH= {args.target_branch} \n"
-                   f"PUSH_ORIGIN= {args.push_origin} \n"
-                   f"PUSH_ENABLED= {args.push_enabled} \n"
-                   f"SOURCE_BRANCH= {args.source_branch} \n"))
+    print(cleandoc(f"""
+                   Commandline parameter
+                   source_dir= {source_dir}
+                   module_path= {str(args.module_path)}
+                   TARGET_BRANCH= {args.target_branch}
+                   PUSH_ORIGIN= {args.push_origin}
+                   PUSH_ENABLED= {args.push_enabled} 
+                   SOURCE_BRANCH= {args.source_branch}""") + "\n")
 
     with TemporaryDirectory() as tempdir:
         deployer = GithubPagesDeployer(source_dir, args.source_branch, current_commit_id, args.module_path,
                                        args.target_branch, args.push_origin, args.push_enabled,
                                        tempdir)
         os.mkdir(deployer.build_dir)
-        print(cleandoc(f"Using following Directories: \n"
-                       f"TMP= {tempdir}\n"
-                       f"TARGET WORKTREE= {deployer.worktree_paths['target_worktree']} \n"
-                       f"SOURCE WORKTREE= {deployer.worktree_paths['source_worktree']} \n"
-                       f"BUILD_DIR= {deployer.build_dir} \n"
-                       f"CURRENT_COMMIT_ID= {deployer.current_commit_id} \n"))
+        print(cleandoc(f"""
+                       Using following Directories:
+                       TMP= {tempdir}
+                       TARGET WORKTREE= {deployer.worktree_paths['target_worktree']}
+                       SOURCE WORKTREE= {deployer.worktree_paths['source_worktree']}
+                       BUILD_DIR= {deployer.build_dir}
+                       CURRENT_COMMIT_ID= {deployer.current_commit_id}""") + "\n")
         try:
             deployer.detect_or_verify_source_branch()
             deployer.checkout_target_branch_as_worktree()
