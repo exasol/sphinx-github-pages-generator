fc723250f4bb55cc880f595268adbbcfb7057711
MarleneKress79789 <marlene.kress@gmx.de>
2022-05-17 12:00:58 +0200

    add comments

--- exasol_sphinx_github_pages_generator/generate_index.py
+++ exasol_sphinx_github_pages_generator/generate_index.py
@@ -5,6 +5,8 @@ import sys
 import glob
 import shutil
 from subprocess import run
+
+import importlib_resources
 from jinja2 import Environment, PackageLoader, select_autoescape
 import inspect
 from typing import List, Dict, Generator, Union, Any
@@ -108,18 +110,25 @@ def get_releases(target_branch: str, target_branch_exists_remote: bool, source_b
     return release_list_dicts
 
 
-def copy_importlib_resources_file(src_file, target_file):
-    try:
-        content = src_file.read_text()
-        with open(target_file, "w") as file:
-            file.write(content)
-    except UnicodeDecodeError:
-        content = src_file.read_bytes()
-        with open(target_file, "wb") as file:
-            file.write(content)
+def copy_importlib_resources_file(src_file: importlib_resources.abc.Traversable, target_file: Path) -> None:
+    """
+    Uses a given source path "src_file" given as an importlib_resources.abc.Traversable to copy the file it points to
+    into the destination denoted by target_path.
+    :param src_file: Location of the file to be copied, given as importlib_resources.abc.Traversable.
+    :param target_file: Path object the location file should be copied to.
+    """
+    content = src_file.read_bytes()
+    with open(target_file, "wb") as file:
+        file.write(content)
 
 
-def copy_importlib_resources_dir_tree(src_path, target_path):
+def copy_importlib_resources_dir_tree(src_path: importlib_resources.abc.Traversable, target_path: Path) -> None:
+    """
+    Uses a given source path "scr_path" given as an importlib_resources.abc.Traversable to copy all files/directories
+    in the directory tree whose root is scr_path into target_path.
+    :param src_path: Root of the dir tree to be copied, given as importlib_resources.abc.Traversable.
+    :param target_path: Path object the dir tree should be copied to.
+    """
     if not target_path.is_dir():
         target_path.mkdir()
     for file in src_path.iterdir():
