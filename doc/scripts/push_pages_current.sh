527a2c05c20db2d1475fd591b2ad372ce3c27aee
MarleneKress79789 <marlene.kress@gmx.de>
2022-05-24 12:36:06 +0200

    wrong quotes

--- doc/scripts/push_pages_current.sh
+++ doc/scripts/push_pages_current.sh
@@ -1,6 +1,6 @@
 declare -a StringArray=("../exasol_sphinx_github_pages_generator" )
 python3 ./exasol_sphinx_github_pages_generator/deploy_github_pages.py  \
-  --target_branch "github-pages/'$(git branch --show-current)'"  \
+  --target_branch "github-pages/"$(git branch --show-current)""  \
   --push_origin "origin"  \
   --push_enabled "push"  \
   --module_path "${StringArray[@]}"
