d2c935485f06886465c1322542d52a11a86b5397
Nicola Coretti <nicola.coretti@exasol.com>
2022-06-22 15:25:29 +0200

    Remove doc scripts
    The doc scripts are replaced by nox actions and python code.

--- doc/scripts/push_pages_current.sh
+++
@@ -1,5 +0,0 @@
-declare -a StringArray=("../exasol_sphinx_github_pages_generator" )
-sgpg --target_branch "github-pages/"$(git branch --show-current)""  \
-  --push_origin "origin"  \
-  --push_enabled "push"  \
-  --module_path "${StringArray[@]}"
--- doc/scripts/push_pages_release.sh
+++
@@ -1,8 +0,0 @@
-declare -a StringArray=("../exasol_sphinx_github_pages_generator" )
-sgpg --target_branch "github-pages/main" \
-  --push_origin "origin" \
-  --push_enabled "push" \
-  --module_path "${StringArray[@]}" \
-  --source_branch "$(git tag --sort=committerdate | tail -1)" \
-  --source_origin "tags"
-
